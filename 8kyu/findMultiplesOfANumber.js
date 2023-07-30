a59f4871a987a77aeeda2048c7425307042e1e94
Ruben Arcos <ruben.arcos06@gmail.com>
2023-07-29 22:28:09 -0500

    codewars 07.29.2023 - used charAt, slice, regex, replace

--- 8kyu/findMultiplesOfANumber.js
+++ 8kyu/findMultiplesOfANumber.js
@@ -6,7 +6,9 @@
 function findMultiples(integer, limit) {
   let arrNums = [];
 
-  for (let i = integer; i <= limit; i += integer) arrNums.push(i);
+  for (let i = integer; i <= limit; i += integer) {
+    arrNums.push(i);
+  }
   return arrNums;
 }
 
